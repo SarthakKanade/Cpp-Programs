0579c9e03935dfd08cdea0d9212d854123f5523e
SarthakKanade <sarthakkanade527@gmail.com>
2024-12-27 22:34:02 +0530

    Update filemanager.cpp

--- filemanager.cpp
+++ filemanager.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 // Structure to store book details
-struct Book {
+struct Book 
+{
     int bookID;          // Book ID
     string bookName;     // Title of the book
     string bookAuthor;   // Author of the book
@@ -13,7 +14,8 @@ struct Book {
     string issuedTo;     // Name of the person who issued the book
 
     // Display book details
-    void showBookDetails() const {
+    void showBookDetails() const 
+{
         cout << "ID: " << bookID
              << ", Name: " << bookName
              << ", Author: " << bookAuthor
@@ -31,7 +33,8 @@ void removeBook(vector<Book>& library);
 void saveLibrary(const vector<Book>& library, const string& fileName);
 void loadLibrary(vector<Book>& library, const string& fileName);
 
-int main() {
+int main() 
+{
     vector<Book> library;   // Dynamic list of books
     int menuChoice;
     const string fileName = "library_data.txt";
@@ -43,7 +46,8 @@ int main() {
     cout << " Library Management Program\n";
     cout << "=============================\n";
 
-    do {
+    do 
+    {
         // Display menu options
         cout << "\n1. Add New Book";
         cout << "\n2. Show All Books";
@@ -55,7 +59,8 @@ int main() {
         cout << "\nEnter your choice: ";
         cin >> menuChoice;
 
-        switch (menuChoice) {
+        switch (menuChoice) 
+        {
             case 1: addNewBook(library); break;
             case 2: showAllBooks(library); break;
             case 3: findBook(library); break;
@@ -71,7 +76,8 @@ int main() {
 }
 
 // Function to add a new book
-void addNewBook(vector<Book>& library) {
+void addNewBook(vector<Book>& library) 
+{
     Book newBook;
     cout << "\nEnter Book ID: ";
     cin >> newBook.bookID;
@@ -88,20 +94,24 @@ void addNewBook(vector<Book>& library) {
 }
 
 // Function to show all books
-void showAllBooks(const vector<Book>& library) {
-    if (library.empty()) {
+void showAllBooks(const vector<Book>& library) 
+{
+    if (library.empty()) 
+    {
         cout << "The library is empty.\n";
         return;
     }
 
     cout << "\nList of Books:\n";
-    for (const auto& book : library) {
+    for (const auto& book : library) 
+    {
         book.showBookDetails();
     }
 }
 
 // Function to find a book
-void findBook(const vector<Book>& library) {
+void findBook(const vector<Book>& library) 
+{
     cin.ignore();
     string searchQuery;
     cout << "\nEnter Book Name or Author to Search: ";
@@ -115,22 +125,29 @@ void findBook(const vector<Book>& library) {
         }
     }
 
-    if (!found) {
+    if (!found) 
+    {
         cout << "No books found matching the search.\n";
     }
 }
 
 // Function to issue a book
-void issueBook(vector<Book>& library) {
+void issueBook(vector<Book>& library) 
+{
     int bookID;
     cout << "\nEnter Book ID to Issue: ";
     cin >> bookID;
 
-    for (auto& book : library) {
-        if (book.bookID == bookID) {
-            if (book.isIssued) {
+    for (auto& book : library) 
+    {
+        if (book.bookID == bookID) 
+        {
+            if (book.isIssued) 
+            {
                 cout << "This book is already issued to " << book.issuedTo << ".\n";
-            } else {
+            } 
+            else 
+            {
                 cin.ignore();
                 cout << "Enter the name of the person issuing the book: ";
                 getline(cin, book.issuedTo);
@@ -149,13 +166,18 @@ void returnIssuedBook(vector<Book>& library) {
     cout << "\nEnter Book ID to Return: ";
     cin >> bookID;
 
-    for (auto& book : library) {
-        if (book.bookID == bookID) {
-            if (book.isIssued) {
+    for (auto& book : library) 
+    {
+        if (book.bookID == bookID) 
+        {
+            if (book.isIssued) 
+            {
                 book.isIssued = false;
                 cout << "Book returned successfully from " << book.issuedTo << ".\n";
                 book.issuedTo = "";
-            } else {
+            } 
+            else 
+            {
                 cout << "This book is not currently issued.\n";
             }
             return;
@@ -165,13 +187,16 @@ void returnIssuedBook(vector<Book>& library) {
 }
 
 // Function to remove a book from the library
-void removeBook(vector<Book>& library) {
+void removeBook(vector<Book>& library) 
+{
     int bookID;
     cout << "\nEnter Book ID to Remove: ";
     cin >> bookID;
 
-    for (auto it = library.begin(); it != library.end(); ++it) {
-        if (it->bookID == bookID) {
+    for (auto it = library.begin(); it != library.end(); ++it) 
+    {
+        if (it->bookID == bookID) 
+        {
             library.erase(it);
             cout << "Book removed from the library.\n";
             return;
@@ -181,10 +206,12 @@ void removeBook(vector<Book>& library) {
 }
 
 // Function to save library data to a file
-void saveLibrary(const vector<Book>& library, const string& fileName) {
+void saveLibrary(const vector<Book>& library, const string& fileName) 
+{
     ofstream outFile(fileName);
 
-    for (const auto& book : library) {
+    for (const auto& book : library) 
+    {
         outFile << book.bookID << "\n"
                 << book.bookName << "\n"
                 << book.bookAuthor << "\n"
@@ -196,14 +223,17 @@ void saveLibrary(const vector<Book>& library, const string& fileName) {
 }
 
 // Function to load library data from a file
-void loadLibrary(vector<Book>& library, const string& fileName) {
+void loadLibrary(vector<Book>& library, const string& fileName) 
+{
     ifstream inFile(fileName);
-    if (!inFile) {
+    if (!inFile) 
+    {
         return; // File does not exist
     }
 
     Book book;
-    while (inFile >> book.bookID) {
+    while (inFile >> book.bookID) 
+    {
         inFile.ignore();
         getline(inFile, book.bookName);
         getline(inFile, book.bookAuthor);
